ab7bf264701deea68c232e72bae5a72d199b60ce
Matthias Mullie <git@mullie.eu>
2012-10-17 18:49:00 +0200

    code maintenance
    Change-Id: If7d8ed95fec06b3a93ec80fbcad2110637f9b9c3

--- CustomData.php
+++ CustomData.php
@@ -4,76 +4,20 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
 }
 
-$wgExtensionFunctions[] = 'wfSetupCustomData';
-$wgExtensionCredits['parserhook'][] = array(
+// autoloader
+$wgAutoloadClasses['CustomData'] = __DIR__ . '/CustomData.hooks.php';
+
+// hooks
+$wgCustomData = new CustomData;
+$wgHooks['ParserClearState'][] = array( &$wgCustomData, 'onParserClearState' );
+$wgHooks['OutputPageParserOutput'][] = array( &$wgCustomData, 'onOutputPageParserOutput' );
+
+// credits
+$wgExtensionCredits['parserhook']['CustomData'] = array(
+	'path' => __FILE__,
 	'name' => 'CustomData',
-	'url' => 'http://wikivoyage.org/tech/CustomData-Extension',
-	'author' => 'Hans Musil'
+	'url' => '//www.mediawiki.org/wiki/Extension:CustomData',
+	'description' => 'Provides an easy to use interface for other extensions that need to transfer data retrieved from the parser to the skin.',
+	'author' => array( 'Hans Musil' ),
+	'version' => '1.1'
 );
-
-
-class CustomData {
-	/*
-	 * $value must be an array.
-	 */
-	function setParserData( &$ParserOutput, $key, $value) {
-		$ParserOutput->mWVcustom[$key] = $value;
-	}
-
-	function getParserData( &$ParserOutput, $key) {
-		return 	array_key_exists( 'mWVcustom', $ParserOutput ) &&
-			array_key_exists( $key, $ParserOutput->mWVcustom )
-			? $ParserOutput->mWVcustom[$key] : array();
-
-		# return $ParserOutput->mWVcustom[ $key];
-	}
-
-	function getPageData( &$OutputPage, $key ) {
-		return $this->getParserData( $OutputPage, $key );
-	}
-
-	function setSkinData( &$QuickTmpl, $key, $value ) {
-		if( !isset( $QuickTmpl->data['WVcustom'] ) ) {
-			$QuickTmpl->data['WVcustom'] = array();
-		}
-		$QuickTmpl->data['WVcustom'][$key] = $value;
-	}
-
-	function getSkinData( &$QuickTmpl, $key ) {
-		return $QuickTmpl->data['WVcustom'][$key];
-	}
-
-	function onParserClearState( &$Parser) {
-		$Parser->mOutput->mWVcustom = array();
-
-		return true;
-	}
-
-	function onOutputPageParserOutput( &$OutputPage, $parserOutput ) {
-		if( !isset( $OutputPage->mWVcustom ) ) {
-			$OutputPage->mWVcustom = array();
-		}
-
-		foreach( $parserOutput->mWVcustom as $k => $v ) {
-			# error_log( '$k = ' . $k, 0);
-			if( !array_key_exists( $k, $OutputPage->mWVcustom ) ) {
-				$OutputPage->mWVcustom[$k] = array();
-			}
-			$OutputPage->mWVcustom[$k] += $v;
-		}
-
-		return true;
-	}
-
-}
-
-function wfSetupCustomData() {
-	global $wgHooks;
-
-	global $wgCustomData;
-	$wgCustomData     = new CustomData;
-
-	$wgHooks['ParserClearState'][] = array( &$wgCustomData, 'onParserClearState' );
-	$wgHooks['OutputPageParserOutput'][] = array( &$wgCustomData, 'onOutputPageParserOutput' );
-}
-
