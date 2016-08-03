74c2b541713f2bf9ed41504ea97eca78c31aba44
Kunal Mehta <legoktm@member.fsf.org>
2016-08-02 17:28:18 -0700

    Archive repository, obsolete
    It was actually replaced by core functionality in MediaWiki 1.21.
    
    Change-Id: I28b78dbf630e7a65cf5906949ca945cc0ce68c8f

--- CustomData.php
+++ CustomData.php
@@ -1,26 +1,6 @@
 <?php
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
-}
+// This extension is obsolete and has been archived. You can use the core
+// functionality of ParserOutput::setExtensionData() since MediaWiki 1.21
+// and the OutputPageParserOutput hook.
 
-// autoloader
-$dir = __DIR__  . '/';
-$wgAutoloadClasses['CustomData'] = $dir . 'CustomData.hooks.php';
-$wgMessagesDirs['CustomData'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['CustomData'] = $dir . 'CustomData.i18n.php';
-
-// hooks
-$wgCustomData = new CustomData;
-$wgHooks['ParserClearState'][] = array( &$wgCustomData, 'onParserClearState' );
-$wgHooks['OutputPageParserOutput'][] = array( &$wgCustomData, 'onOutputPageParserOutput' );
-
-// credits
-$wgExtensionCredits['parserhook'][] = array(
-	'path' => __FILE__,
-	'name' => 'CustomData',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:CustomData',
-	'descriptionmsg' => 'customdata-desc',
-	'author' => array( 'Hans Musil' ),
-	'version' => '1.2'
-);
